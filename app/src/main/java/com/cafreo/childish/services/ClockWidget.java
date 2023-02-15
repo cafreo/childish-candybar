5d32a6ff0d67686329ba938917b95b784661abaa
PUPPY <taehyun_uses_tools.se9ou@slmail.me>
2023-02-15 15:43:26 +0100

    Initial commit

---
+++ app/src/main/java/com/cafreo/childish/services/ClockWidget.java
@@ -0,0 +1,5 @@
+package com.cafreo.childish.services;
+
+import candybar.lib.services.CandyBarWidgetService;
+
+public class ClockWidget extends CandyBarWidgetService {}
---
+++ app/src/main/java/com/cafreo/childish/services/MuzeiService.java
@@ -0,0 +1,5 @@
+package com.cafreo.childish.services;
+
+import candybar.lib.services.CandyBarMuzeiService;
+
+public class MuzeiService extends CandyBarMuzeiService {}
